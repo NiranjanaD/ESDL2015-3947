ef779c8f443425d15e95cdf7a1e7bc8a0b7a50f0
NiranjanaD <NiranjanaD@users.noreply.github.com>
2015-08-19 14:25:07 +0530

    Create calci.cpp
    addition of two numbers

---
+++ calci.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+int a,b;
+cout<<"Hello, World!"<<endl;
+cout<<"Enter number 1:"<<endl;
+cin>> a;
+cout<<"Enter number 2:"<<endl;
+cin>>b;
+int c=0;
+c=a+b;
+cout<<"Addition is: "<<endl;
+cout<<c;
+return 1;
+}
